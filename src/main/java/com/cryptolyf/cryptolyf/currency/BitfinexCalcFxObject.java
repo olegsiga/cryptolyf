564045fcaf339576e3ff1646cdaa7abdbc2dc746
olegsmatv <olegs.matvijenko@gmail.com>
2021-03-14 22:59:51 +0200

    BitfinexPriceResource getting ready to replace BitfinexResourse

---
+++ src/main/java/com/cryptolyf/cryptolyf/currency/BitfinexCalcFxObject.java
@@ -0,0 +1,35 @@
+package com.cryptolyf.cryptolyf.currency;
+
+public class BitfinexCalcFxObject {
+    private String ccy1;
+    private String ccy2;
+
+    public BitfinexCalcFxObject(String ccy1, String ccy2) {
+        this.ccy1 = ccy1;
+        this.ccy2 = ccy2;
+    }
+
+    public String getCcy1() {
+        return ccy1;
+    }
+
+    public void setCcy1(String ccy1) {
+        this.ccy1 = ccy1;
+    }
+
+    public String getCcy2() {
+        return ccy2;
+    }
+
+    public void setCcy2(String ccy2) {
+        this.ccy2 = ccy2;
+    }
+
+    @Override
+    public String toString() {
+        return "BitfinexCalcFxObject{" +
+                "ccy1='" + ccy1 + '\'' +
+                ", ccy2='" + ccy2 + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/cryptolyf/cryptolyf/currency/BitfinexPriceResource.java
+++ src/main/java/com/cryptolyf/cryptolyf/currency/BitfinexPriceResource.java
@@ -1,6 +1,8 @@
 package com.cryptolyf.cryptolyf.currency;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +12,13 @@ import org.springframework.web.client.RestTemplate;
 public class BitfinexPriceResource {
     @Autowired
     private RestTemplate restTemplate;
+
+    @PostMapping
+    public void calcFx(@RequestBody BitfinexCalcFxObject bitfinexCalcFxObject) {
+        String url = "https://api-pub.bitfinex.com/v2/calc/fx";
+        String value = restTemplate.postForEntity(url, bitfinexCalcFxObject, String.class).getBody();
+        System.out.println(value);
+    }
 
 
 }
