c0981c8bde29ad550e77fbb4cb6b60e911b64cdc
olegsmatv <olegs.matvijenko@gmail.com>
2021-03-16 00:18:00 +0200

    refactor

--- src/main/java/com/cryptolyf/cryptolyf/currency/service/BitfinexService.java
+++ src/main/java/com/cryptolyf/cryptolyf/currency/service/BitfinexService.java
@@ -2,17 +2,12 @@ package com.cryptolyf.cryptolyf.currency.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-//This class is not ready to use
 @Service
 public class BitfinexService {
     @Autowired
@@ -21,10 +16,10 @@ public class BitfinexService {
     public BigDecimal getLastPrice(String ccy1, String ccy2) {
         String url = "https://api-pub.bitfinex.com/v2/calc/fx";
         Map<String, String> params = new HashMap<>();
-        params.put("ccy1",ccy1);
-        params.put("ccy2",ccy2);
+        params.put("ccy1", ccy1);
+        params.put("ccy2", "EUR");
         BigDecimal[] value = restTemplate.postForEntity(url, params, BigDecimal[].class).getBody();
         //System.out.println(value[0]);
-        return value[0];
+        return value != null ? value[0] : null;
     }
 }
