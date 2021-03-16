dcce7b232226fd09ff9672406a5672b4b7c442af
olegsmatv <olegs.matvijenko@gmail.com>
2021-03-16 11:17:43 +0200

    scheduler implemented

--- src/main/java/com/cryptolyf/cryptolyf/CryptolyfApplication.java
+++ src/main/java/com/cryptolyf/cryptolyf/CryptolyfApplication.java
@@ -1,19 +1,25 @@
 package com.cryptolyf.cryptolyf;
 
+import com.cryptolyf.cryptolyf.currency.scheduledTasks.ScheduledValueUpdate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class CryptolyfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CryptolyfApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CryptolyfApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
+
+
---
+++ src/main/java/com/cryptolyf/cryptolyf/currency/scheduledTasks/ScheduledValueUpdate.java
@@ -0,0 +1,35 @@
+package com.cryptolyf.cryptolyf.currency.scheduledTasks;
+
+import com.cryptolyf.cryptolyf.currency.model.Currency;
+import com.cryptolyf.cryptolyf.currency.repository.CurrencyRepository;
+import com.cryptolyf.cryptolyf.currency.service.BitfinexService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+public class ScheduledValueUpdate {
+    private final CurrencyRepository currencyRepository;
+    private final BitfinexService bitfinexService;
+
+    public ScheduledValueUpdate(CurrencyRepository currencyRepository, BitfinexService bitfinexService) {
+        this.currencyRepository = currencyRepository;
+        this.bitfinexService = bitfinexService;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void updateCurrencyValue() {
+        List<Currency> currencies = currencyRepository.findAll();
+        System.out.println("hello world");
+        for (Currency i: currencies) {
+            BigDecimal latestMarketValue = bitfinexService.getLastPrice(i.getName(), "EUR").multiply(i.getAmount());
+            i.setValue(latestMarketValue);
+            System.out.println(latestMarketValue);
+            currencyRepository.save(i);
+        }
+
+    }
+
+}
